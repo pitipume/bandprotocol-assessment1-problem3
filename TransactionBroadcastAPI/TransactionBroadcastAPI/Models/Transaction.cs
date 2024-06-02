cd1a3090fa48f72245cb47fab07d8e83a2291161
pitipume <pitipume.boonyawan@gmail.com>
2024-06-02 21:45:31 +0700

    doc(transaction): create Transaction Model

---
+++ TransactionBroadcastAPI/TransactionBroadcastAPI/Models/Transaction.cs
@@ -0,0 +1,9 @@
+﻿namespace TransactionBroadcastAPI.Models
+{
+    public class Transaction
+    {
+        public string? Symbol { get; set; }
+        public uint Price { get; set; }
+        public uint TimeStamp { get; set; }
+    }
+}
